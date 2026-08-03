4dfa7944a28af613dbb98c893c37ad2334e09997
Juan Pablo Gonzales <juampi7237@gmail.com>
2026-08-03 14:40:44 -0400

    aplicando strategy method en los diferentes tipos de Fee

--- src/main/java/com/example/atlas_bank/transaction/service/fee/CheckingFeeCalculator.java
+++ src/main/java/com/example/atlas_bank/transaction/service/fee/CheckingFeeCalculator.java
@@ -1,10 +1,12 @@
 package com.example.atlas_bank.transaction.service.fee;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
+@Order(1)
 public class CheckingFeeCalculator implements FeeCalculator{
     @Override
     public boolean supports(String accountType) {
--- src/main/java/com/example/atlas_bank/transaction/service/fee/DefaultFeeCalculator.java
+++ src/main/java/com/example/atlas_bank/transaction/service/fee/DefaultFeeCalculator.java
@@ -1,10 +1,12 @@
 package com.example.atlas_bank.transaction.service.fee;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
+@Order() //como no tiene un valor lo pone al ultimo de la lista
 public class DefaultFeeCalculator implements FeeCalculator{
     @Override
     public boolean supports(String accountType) {
